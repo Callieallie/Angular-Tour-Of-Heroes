4418cc5c2817d18d2dcda863627c740674af4746
Callieallie <callieacts@gmail.com>
2023-08-01 15:22:40 -0400

    Added title component and css defaults

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
+
+function printString(message: string) {
+  console.log(message);
+}
+
+/**
+ * Anything can be set on a js object ({})
+ * Callable objects
+ * Classes are just a js object nut
+ * Decorators are functions that take object/function nuts and transform them
+ */
